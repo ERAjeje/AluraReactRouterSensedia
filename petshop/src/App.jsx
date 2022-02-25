bba84fc14f810012a18db7a0f3e37de62fbae62e
Edson Ajeje <edson.ajeje@terra.com.br>
2022-02-25 08:41:04 -0300

    curso: criação da função router

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './assets/css/base/base.css'
+import Home from './paginas/Home'
+import Sobre from './paginas/Sobre'
+
 function App() {
+
+  const Router = () => {
+    const location = window.location.pathname;
+    if (location === '/sobre') {
+      return <Sobre />
+    }
+    return <Home />
+  };
 
   return (
     <>
+      {Router()}
     </>
   )
 }
