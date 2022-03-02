6db157f2d589bfba92af759de034bd3553551098
Edson Ajeje <edson.ajeje@terra.com.br>
2022-03-02 15:50:08 -0300

    curso: criacao comp subcategoria e rend condicion

--- src/paginas/Categoria.jsx
+++ src/paginas/Categoria.jsx
@@ -1,22 +1,48 @@
-import React from "react";
-import { useRouteMatch } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, Switch, useRouteMatch } from "react-router-dom";
 import { Route, useParams } from "react-router-dom";
+import { search } from "../api/api";
 import '../assets/css/blog.css';
 import ListaCategorias from "../components/ListaCategorias";
 import ListaPost from "../components/ListaPost";
+import SubCategoria from "../components/SubCategoria";
 
 const Categoria = () => {
     const { id } = useParams();
-    const { path } = useRouteMatch();
+    const { path, url } = useRouteMatch();
+
+    const [subCategoria, setSubCategoria] = useState([]);
+
+    useEffect(() => {
+        search(`/categorias/${id}`, (categoria) => {
+            setSubCategoria(categoria.subcategorias);
+        })
+    }, [id]);
+
     return (
         <>
             <div className="container">
                 <h2 className="titulo-pagina">Pet Noticias</h2>
             </div>
             <ListaCategorias />
-            <Route exact path={`${path}/`}>
-                <ListaPost url={`/posts?categoria=${id}`} />
-            </Route>
+            <ul className="lista-categorias container flex">
+                {
+                    subCategoria.map(subcategoria =>
+                        <li className={`lista-categorias__categoria lista-categorias__categoria--${id}`}
+                            key={subcategoria}>
+                            <Link to={`${url}/${subcategoria}`}>{subcategoria}</Link>
+                        </li>
+                    )
+                }
+            </ul>
+            <Switch>
+                <Route exact path={`${path}/`}>
+                    <ListaPost url={`/posts?categoria=${id}`} />
+                </Route>
+                <Route path={`${path}/:subcategoria`}>
+                    <SubCategoria />
+                </Route>
+            </Switch>
         </>
     );
 }
