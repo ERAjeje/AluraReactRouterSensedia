7d2f9ece28ddf024a651522680c05c940a82a148
Edson Ajeje <edson.ajeje@terra.com.br>
2022-03-02 14:56:02 -0300

    curso: criacao comp ListaPost

--- src/components/ListaPost.jsx
+++ src/components/ListaPost.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { search } from "../api/api";
+import { normalizeUrl } from "../utils/normalize";
 
-const ListaPost = () => {
+const ListaPost = ({ url }) => {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => { search(url, setPosts) }, []);
+
     return (
         <section className="posts container">
-
+            {
+                posts.map(post =>
+                    <Link to={`/posts/${post.id}`}
+                        key={post.id} className={`cartao-post cartao-post--${post.categoria}`}>
+                        <article>
+                            <h3 className="cartao-post__titulo">{post.title}</h3>
+                            <p className="cartao-post__meta">{post.metadescription}</p>
+                        </article>
+                    </Link>
+                )
+            }
         </section>
     );
 }
